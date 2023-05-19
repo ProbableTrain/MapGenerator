607e22eb8a5d9664e7c1184f7f4c7827ec9abd31
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-05-19 12:34:07 +0200

    Update model_generator.ts

--- src/ts/model_generator.ts
+++ src/ts/model_generator.ts
@@ -2,7 +2,7 @@ import * as log from 'loglevel';
 //https://threejs.org/manual/#en/fundamentals
 import * as THREE from 'three';
 import { CSG } from 'three-csg-ts';
-import * as exportSTL from './exporter/STLExporter';
+import {STLExporter } from './exporter/STLExporter.js';
 import Vector from './vector';
 import {BuildingModel} from './ui/buildings';
 
